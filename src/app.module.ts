ba03f42bc174e77b3f95d6851476ca7172db054c
saxxone <saxxone17@gmail.com>
2021-03-04 17:34:37 +0100

    tweak for mysql conn

--- src/app.module.ts
+++ src/app.module.ts
@@ -16,7 +16,7 @@ import { InviteesModule } from './invitees/invitees.module';
       port: 3306,
       username: 'root', //for localhost
       // username: 'admin', // for aws
-      password: '3e63UUMGIuuJvKb9',
+      password: '#KV9%e_VcaEfPjKy',
       database: 'tmcdb',
       entities: ['dist/**/*.entity{.ts,.js}'],
       synchronize: true,
@@ -29,6 +29,6 @@ import { InviteesModule } from './invitees/invitees.module';
 })
 export class AppModule {}
 
-//PROMPT> mysql -h database-2.cg3cfi29nmy8.us-east-2.rds.amazonaws.com -P 3306 -u admin -p 3e63UUMGIuuJvKb9
+//PROMPT> mysql -h database-2.cg3cfi29nmy8.us-east-2.rds.amazonaws.com -P 3306 -u admin -p #KV9%e_VcaEfPjKy
 //globroot credentials for DO
 //CREATE USER 'globroot'@'localhost' IDENTIFIED BY '3e#@63UUMGIuuJvKb9';
